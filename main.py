0641901143fc1abeaf1ecfdd55f1566b4e29ce0f
Ayush Varshney <ayush.varshney@tothenew.com>
2021-05-14 18:48:27 +0530

    committed Face Recognition API

--- main.py
+++ main.py
@@ -14,12 +14,11 @@ def restAPI():
         for filename in os.listdir(folder):
             images.append(filename)
         return images
-    video_capture = cv2.VideoCapture(0)
-    images = load_images_from_folder("/home/ayush/Documents/homepage/images")
+    images = load_images_from_folder("/var/www/html/Face-Recognition-Rest_API/images")
     # print(images)
     images_name = []
     for img in images:
-        images_name.append(fr.load_image_file(os.path.join("/home/ayush/Documents/homepage/images", img)))
+        images_name.append(fr.load_image_file(os.path.join("/var/www/html/Face-Recognition-Rest_API/images", img)))
     encodings = []
     for img in images_name:
         encodings.append(fr.face_encodings(img)[0])
@@ -28,21 +27,23 @@ def restAPI():
     for encode in encodings:
         known_face_encodings.append(encode)
     known_face_names = []
-
+    
     for name in images:
         known_face_names.append(os.path.splitext(name)[0])
+    # print(known_face_names)
     # Initialize some variables
 
     face_locations = []
     face_encodings = []
     face_names = []
     process_this_frame = True
+    video_capture = cv2.VideoCapture(0)
     while True:
         # Grab a single frame of video
         ret, frame = video_capture.read()
 
         # Resize the frame of video to 1/4 size for fast process
-        small_frame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
+        small_frame = cv2.resize(frame, (0,0), fx=0.25, fy=0.25, interpolation=cv2.INTER_AREA)
         # small_frame = cv2.resize(frame, (0, 0), fx=0.5, fy=0.5) 
         # Convert the image from BGR color(opencv) to RGB color(face_recognition)
         rgb_small_frame = small_frame[:, :, ::-1]
@@ -50,17 +51,16 @@ def restAPI():
         if process_this_frame:
             # Find all the faces and face encodings in the current frame of video
             face_locations = fr.face_locations(rgb_small_frame)
-            face_encodings = fr.face_encodings(rgb_small_frame, face_locations)
-            face_names = []
-            for face_encoding in face_encodings:
-                # See if the face is a match for known face(s)
-                matches = fr.compare_faces(known_face_encodings, face_encoding)
-                name = "Unknown"
-                face_distances = fr.face_distance(
-                    known_face_encodings, face_encoding)
-                best_match_index = np.argmin(face_distances)
-                if matches[best_match_index]:
-                    name = known_face_names[best_match_index]
+        face_encodings = fr.face_encodings(rgb_small_frame, face_locations)
+        face_names = []
+        for face_encoding in face_encodings:
+            # See if the face is a match for known face(s)
+            matches = fr.compare_faces(known_face_encodings, face_encoding)
+            name = "Unknown"
+            face_distances = fr.face_distance(known_face_encodings, face_encoding)
+            best_match_index = np.argmin(face_distances)
+            if matches[best_match_index]:
+                name = known_face_names[best_match_index]
                 face_names.append(name)
             # print(face_names)
         process_this_frame = not process_this_frame
@@ -77,6 +77,7 @@ def restAPI():
             cv2.putText(frame, name, (left+6, bottom-6),
                         font, 1.0, (225, 225, 225), 1)
         cv2.imshow('Video', frame)
+        # if cv2.waitKey(1) & 0XFF==ord('q'):
         if cv2.waitKey(1) & 0XFF==ord('q'):
             break
     video_capture.release()
@@ -89,4 +90,4 @@ def restAPI():
     return jsonify(result)
 
 if __name__ == "__main__":
-    app.run(host='127.0.0.1', port=9000, debug=True)
+    app.run(host='127.0.0.1', port=2000, debug=True)
