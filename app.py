501852ecc7c13d52aa3c5747a4aa4f5ae3ee4f65
ayush.varshney <ayush.varshney@tothenew.com>
2021-06-01 12:59:28 +0530

    Committed Face Recognition

--- app.py
+++ app.py
@@ -91,5 +91,5 @@ def restAPI():
     return jsonify(result)
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 17995))
+    port = int(os.environ.get("PORT", 17996))
     app.run(host='0.0.0.0', port=port)
